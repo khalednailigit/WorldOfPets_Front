3f819bbfe5a5d0d787066b988d2c3b07cab9c4d6
fatmahendaouiesprit <86419855+fatmahendaouiesprit@users.noreply.github.com>
2021-07-26 19:36:37 +0100

    Integration part signup done

--- src/app/modules/auth/_services/auth.service.ts
+++ src/app/modules/auth/_services/auth.service.ts
@@ -93,9 +93,11 @@ export class AuthService implements OnDestroy {
   registration(user): Observable<any> {
     user.CreateAt=this.today;
     user.UpdateAt=this.today;
+    // user.roles='["ROLE_USER"]';
+    user.username='';
+
 		let headers = new HttpHeaders();
     headers = headers.append("Content-Type", "application/json");
-    headers = headers.append("Authorization",'Bearer '+`${localStorage.getItem('token')}`);
 		return this.http.post(environment.API_URL + "api/addUtilisateur",
 			user, { headers: headers }).pipe(map(res => {
 				return res;
--- src/app/modules/user-management/_services/user.service.ts
+++ src/app/modules/user-management/_services/user.service.ts
@@ -52,10 +52,12 @@ export class UserService {
 	}
   create(user): Observable<any> {
     user.CreateAt=this.today;
-    user.UpdateAt=this.today;
+	user.UpdateAt=this.today;
+	// user.roles='["ROLE_USER"]';
+	user.username='';
+
 		let headers = new HttpHeaders();
 		headers = headers.append("Content-Type", "application/json");
-		headers = headers.append("Authorization", 'Bearer ' + `${localStorage.getItem('token')}`);
 		return this.http.post(environment.API_URL + "api/addUtilisateur",
 			user, { headers: headers }).pipe(map(res => {
 				return res;
